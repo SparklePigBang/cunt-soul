45b90fc98b9628cc5a2a50330b5ff9dc5bb4388f
Sam Adam-Day <me@samadamday.com>
2014-09-11 20:40:45 +0100

    Removed CD

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,4 @@
 # Creates a PDF out of the README.md file
 
-cd ..
-pandoc -f markdown_github -t pdf -o rules.pdf README.md
+pandoc -f markdown_github -o rules.pdf README.md
 pandoc -f markdown_github -t html5 -o rules.html README.md
