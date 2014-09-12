7dacf3c72dab32b495b2530ef60bdd37b75a0747
Sam Adam-Day <me@samadamday.com>
2014-09-12 12:18:44 +0100

    Finished making bash build script

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,33 @@
-# Creates a PDF out of the README.md file
+# Creates an HTML document and a PDF out of the README.md file
+# Requirements: pandoc pdflatex
 
-pandoc -f markdown_github -o rules.pdf README.md
-pandoc -f markdown_github -t html5 -o rules.html README.md
+# Create a temporary directory, deleting the non-directory file `tmp` if it exists 
+if [ ! -d tmp ]; then
+	if [ -e tmp ]; then rm tmp; fi
+	mkdir tmp
+fi
+
+# Generate the PDF by generating the latex file, making sure tables wrap, and then using pdflatex
+pandoc \
+	-f markdown_github \
+	-o tmp/rules.latex \
+	--variable geometry="margin=2cm" \
+	--variable fontfamily="bookman" \
+	--variable table-second-width="16cm" \
+	--standalone \
+	--template=templates/pandoc-template.latex \
+	README.md
+sed -i 's/{@{}ll@{}}/{@{}lp{16cm}@{}}/g' tmp/rules.latex
+pdflatex -output-directory tmp rules.latex
+mv -f tmp/rules.pdf .
+
+# Generate the HTML page
+pandoc \
+	-f markdown_github \
+	-t html5 \
+	-o rules.html \
+	--smart \
+	README.md
+
+# Clean up
+rm -r tmp
